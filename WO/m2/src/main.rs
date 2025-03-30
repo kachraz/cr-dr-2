31553071102a09e0f568e996024fd8f11eddead2
kachraz <110726496+kachraz@users.noreply.github.com>
2025-03-30 17:28:07 +0000

    🫦

---
+++ WO/m2/src/main.rs
@@ -0,0 +1,9 @@
+// main entry point
+
+mod ut;
+mod w1;
+use w1::w1_main;
+
+fn main() {
+    w1_main();
+}
---
+++ WO/m2/src/w1.rs
@@ -0,0 +1,11 @@
+// work one
+
+use crate::ut::{clear_console, header, print_with_synthwave_gradient};
+
+pub fn w1_main() {
+    clear_console();
+    print_with_synthwave_gradient("booty".to_string());
+    header("W3");
+    println!("W1 is a module that does something interesting.");
+    println!("It is currently under development.");
+}
