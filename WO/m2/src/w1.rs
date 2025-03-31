3ba28a4670ea39aaf907b4998a69bed063b61b48
kachraz <110726496+kachraz@users.noreply.github.com>
2025-03-31 08:22:24 +0000

    🫦

--- WO/m2/src/w1.rs
+++ WO/m2/src/w1.rs
@@ -1,6 +1,12 @@
 // work one
+#![allow(unused)]
+#![allow(dead_code)]
 
 use crate::ut::{clear_console, header, print_with_synthwave_gradient};
+
+//////////////////////////////////////////
+/// Main Entry Function
+//////////////////////////////////////////
 
 pub fn w1_main() {
     clear_console();
@@ -8,4 +14,16 @@ pub fn w1_main() {
     header("W3");
     println!("W1 is a module that does something interesting.");
     println!("It is currently under bootysmells.");
+}
+
+//////////////////////////////////////////
+/// Functions Areas  
+//////////////////////////////////////////
+
+/// Struct function
+
+struct Person {
+    first_name: String,
+    last_name: String,
+    age: u32,
 }
