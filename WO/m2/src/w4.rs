8b080a8c7c5b3186a60d8e613f27c5635a29f894
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-01 13:02:45 +0000

    🫦

--- WO/m2/src/w4.rs
+++ WO/m2/src/w4.rs
@@ -182,12 +182,19 @@ enum FileSize {
 }
 
 fn enum5_format_size(size: u64) -> String {
-    let file_size = match size {
-        0..999 => FileSize::Bytes(size),
-        1000..999_999 => FileSize::Kilobytes(size / 1000),
+    let filesize = match size {
+        0..=999 => FileSize::Bytes(size),
+        1000..=999_999 => FileSize::Kilobytes(size / 1000),
         1_000_000..999_999_999 => FileSize::Megabytes(size / 1_000_000),
         _ => FileSize::Gigabytes(size / 1_000_000_000),
     };
+
+    match filesize {
+        FileSize::Bytes(b) => format!("{} Bytes", b),
+        FileSize::Kilobytes(kb) => format!("{:.2} KB", kb as f64 / 1000.0),
+        FileSize::Megabytes(mb) => format!("{:.2} MB", mb kb as f64 / 1000.0),
+        FileSize::Gigabytes(gb) => format!("{:.2} GB", gb kb as f64 / 1000.0),
+    }
 }
 
 fn enum_5() {}
