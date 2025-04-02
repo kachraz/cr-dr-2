6c5584d76a285e6baecce835c2b2004f77c456f6
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-02 14:31:32 +0000

    🫦

--- WO/m4lib/src/lib.rs
+++ WO/m4lib/src/lib.rs
@@ -11,6 +11,14 @@ use ut::{clear_console, divy, header, print_with_synthwave_gradient};
 // Main function
 
 // Reading from stdin which is the cli
+
+/// Main Panty Smeller -
+/// Reads input from stdin
+/// Tells u to fuck off if you get fucked
+/// # Example
+/// ```
+/// let input = read_stdin()
+/// ```
 pub fn read_stdin() -> String {
     let stdin = std::io::stdin();
     let mut reader = BufReader::new(stdin.lock());
