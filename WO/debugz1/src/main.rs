b14484145b77dcdd4637b52187995305b558e5d5
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-02 18:19:14 +0000

    🫦

--- WO/debugz1/src/main.rs
+++ WO/debugz1/src/main.rs
@@ -1,3 +1,20 @@
+/*
+Simple program that loops over until you type stop
+intentiionally has bugs
+*/
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    while input.trim() != "stop" {
+        input.clear();
+        println!("Please enter a word (type stop to exit)");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        println!("You entered: {}", input.trim());
+    }
+
+    println!("Lick her ass")
 }
