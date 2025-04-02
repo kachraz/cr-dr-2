f9194eee12deba918016b11950057c554c6f4bc0
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-02 14:21:20 +0000

    🫦

--- WO/m4lib/src/lib.rs
+++ WO/m4lib/src/lib.rs
@@ -15,6 +15,8 @@ pub fn read_stdin() -> String {
     let stdin = std::io::stdin();
     let mut reader = BufReader::new(stdin.lock());
     let mut line = String::new();
-    reader.read_line(&mut line).expect("Failed to read line");
+    reader
+        .read_line(&mut line)
+        .expect(" ❌ Failed to read line");
     line.trim().to_string()
 }
