afcf3c6173e3e789023bbabcce70d5de6bcb4195
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-02 14:12:53 +0000

    🫦

--- WO/m4lib/src/lib.rs
+++ WO/m4lib/src/lib.rs
@@ -2,4 +2,12 @@
 Module4 - Learning the library
 */
 
-use ut::{clear_console, divy, print_with_synthwave_gradient};
+mod ut;
+
+use ut::{clear_console, divy, header, print_with_synthwave_gradient};
+
+// Main function
+
+pub fn read_stdin() -> String {
+    let stdin = std::io::stdin();
+}
