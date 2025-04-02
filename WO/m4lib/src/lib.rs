f0fb2e213415c400e880bed313f3d3d21e6efe4b
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-02 14:14:34 +0000

    🫦

--- WO/m4lib/src/lib.rs
+++ WO/m4lib/src/lib.rs
@@ -4,10 +4,15 @@ Module4 - Learning the library
 
 mod ut;
 
+use std::io::{BufRead, BufReader};
+
 use ut::{clear_console, divy, header, print_with_synthwave_gradient};
 
 // Main function
 
 pub fn read_stdin() -> String {
     let stdin = std::io::stdin();
+    let mut reader = BufReader::new(stdin.lock());
+    let mut line = String::new();
+    reader.read_line(&mut line).expect("Failed to read line");
 }
