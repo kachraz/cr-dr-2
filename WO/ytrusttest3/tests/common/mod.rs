76083368cb5feacc08044897fce69025efe7d587
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-03 15:27:07 +0000

    🫦

--- WO/ytrusttest3/tests/common/mod.rs
+++ WO/ytrusttest3/tests/common/mod.rs
@@ -3,7 +3,7 @@ Setting up tests in their own folder for organization
 */
 
 use lolcrab::{Gradient, Lolcrab};
-use std::io::{self, Write}; // Import Write to ensure stdout works properly.
+use std::io;
 
 pub fn setup() {
     mecol();
@@ -12,12 +12,12 @@ pub fn setup() {
 
 fn mecol() -> Result<(), Box<dyn std::error::Error>> {
     let text = "Hello, colorful world!";
-    let mut stdout = io::stdout();
+    let stdout = io::stdout();
     let mut stdout = stdout.lock();
 
     let mut lol = Lolcrab::new(Some(Gradient::Rainbow), None);
 
-    println!("{}", lol.colorize_str(text, &mut stdout));
+    lol.colorize_str(text, &mut stdout)?;
 
     Ok(())
 }
