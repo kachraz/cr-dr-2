ac9c0a81b93b4a6082243534ad7e3ca4541037e9
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-03 15:47:25 +0000

    🫦

--- WO/ytrusttest3/tests/common/mod.rs
+++ WO/ytrusttest3/tests/common/mod.rs
@@ -20,6 +20,8 @@ fn mecol() -> Result<(), Box<dyn std::error::Error>> {
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
 
+    let mut lol = Lolcrab::new(None, None);
+
     lol.colorize_str(TEXT, &mut stdout)?;
 
     Ok(())
