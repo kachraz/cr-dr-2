0b001f5b3385112a8c77c75e2d077de11967d984
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-03 09:09:12 +0000

    🫦

--- WO/ytrusttest2/src/lib.rs
+++ WO/ytrusttest2/src/lib.rs
@@ -1,5 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {a}");
+    10
 }
 
 #[cfg(test)]
@@ -7,8 +8,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_prints_and_returns_10() {
+        let result = prints_and_returns_10(5);
+        assert_eq!(result, 10);
     }
 }
