c2123901f4e11ccffd3c9219911da290d314036e
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-03 06:20:39 +0000

    🫦

--- WO/ytt1/src/lib.rs
+++ WO/ytt1/src/lib.rs
@@ -1,3 +1,16 @@
+/* How to write tests in rust
+
+Tests in rust are written within the same fiel as the code they are testing.
+The bodies oftest sare marked with the #[test] attribute,
+and they perform three main tasks:
+
+1. Setup any needed data or state
+2. Run the code you want to test
+3. Asset the results are what you expect
+
+
+*/
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
