163c29b0c4388df8a59a5e90a10b108f760e0727
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-03 15:22:02 +0000

    🫦

--- WO/ytrusttest3/tests/common/mod.rs
+++ WO/ytrusttest3/tests/common/mod.rs
@@ -9,19 +9,3 @@ pub fn setup() {
     mecol();
     println!("🍫🍫🍫🍫🍫🍫🍫🍫Setting up tests");
 }
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let text = "Hello, colorful world!";
-    let mut stdout = io::stdout();
-
-    // Create a Lolcrab instance with a Rainbow gradient and default noise.
-    let mut lol = Lolcrab::new(Some(Gradient::Rainbow), None);
-
-    // Apply the gradient effect to the text
-    let colorful_text = lol.colorize(text);
-
-    // Write the colorful text to stdout
-    writeln!(stdout, "{}", colorful_text)?;
-
-    Ok(())
-}
