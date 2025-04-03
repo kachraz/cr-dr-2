b0349eb97747259928ca3c231e9060086442c0e7
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-03 01:31:06 +0000

    🫦

---
+++ WO/m4kt/src/lib.rs
@@ -0,0 +1,32 @@
+//! Fuck off bastard
+//! This is for sucking and fucking
+//! Fuck all night
+//! ```
+//! use cli_utils::read_stding
+//! let input = read_stdin()
+//! ```
+//! Smell her ass
+
+/*
+Module4 - Learning the library
+*/
+
+mod ut;
+
+use std::io::{BufRead, BufReader};
+
+use ut::{clear_console, divy, header, print_with_synthwave_gradient};
+
+// Main function
+
+// Reading from stdin which is the cli
+
+pub fn read_stdin() -> String {
+    let stdin = std::io::stdin();
+    let mut reader = BufReader::new(stdin.lock());
+    let mut line = String::new();
+    reader
+        .read_line(&mut line)
+        .expect(" ❌ Failed to read line");
+    line.trim().to_string()
+}
