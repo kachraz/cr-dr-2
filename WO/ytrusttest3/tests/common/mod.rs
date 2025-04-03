0d40d3e54f177e13b2ca424367f58f2c1272ffe6
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-03 15:33:53 +0000

    🫦

--- WO/ytrusttest3/tests/common/mod.rs
+++ WO/ytrusttest3/tests/common/mod.rs
@@ -11,15 +11,8 @@ pub fn setup() {
 }
 const TEXT: &str = "\
 •••••••••••••••••••••••••••••••••••••••••••
-••442463299144744830108724702438783348716••
-••665891426009540978622724448305819269356••
-••078289454141226451790882961903610719673••
 ••56505384476•••••••••••••••••39761609699••
 ••47928752907•• { lolcrab } ••33810561851••
-••51609982385•••••••••••••••••43459368213••
-••980457234663167653959566555465520046709••
-••677103598707232478714861999441705454744••
-••012721882924436718718457599087686681354••
 •••••••••••••••••••••••••••••••••••••••••••
 ";
 
@@ -28,17 +21,9 @@ fn mecol() -> Result<(), Box<dyn std::error::Error>> {
     let mut stdout = stdout.lock();
 
     // Initialize Lolcrab using default gradient and default noise
-    let mut lol = Lolcrab::new(None, None);
+    let mut lol = Lolcrab::new(spectral, None);
 
     lol.colorize_str(TEXT, &mut stdout)?;
-
-    // lol.set_invert(true);
-    // lol.randomize_position();
-    // lol.colorize_str(TEXT, &mut stdout)?;
-
-    // lol.set_invert(false);
-    // lol.reset_position();
-    // lol.colorize_str(TEXT, &mut stdout)?;
 
     Ok(())
 }
