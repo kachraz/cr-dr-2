844b07af6bf33db6f1339e0bb046d77ea0954341
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-03 09:10:53 +0000

    🫦

--- WO/ytrusttest2/src/lib.rs
+++ WO/ytrusttest2/src/lib.rs
@@ -8,8 +8,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_prints_and_returns_10() {
-        let result = prints_and_returns_10(5);
-        assert_eq!(result, 10);
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
     }
 }
