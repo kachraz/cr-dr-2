a110d648edbf5588f11724e627275f59cbd0d95b
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-03 15:11:53 +0000

    🫦

--- WO/ytrusttest3/tests/common/mod.rs
+++ WO/ytrusttest3/tests/common/mod.rs
@@ -2,8 +2,15 @@
 Setting up tests in their own folder for organization
 */
 
-use ::lolcrab::Gradient;
+use lolcrab::{Gradient, Rainbow};
 
 pub fn setup() {
+    let text = "Hello, colorful world!";
+    let gradient = Gradient::Rainbow; // You can choose other gradients like 'Lolcrab', 'Fire', etc.
+    let rainbow = Rainbow::new(gradient, 0.1, 0.0);
+    for (colored_char, _) in rainbow.wrap(text.chars()) {
+        print!("{}", colored_char);
+    }
+    println!();
     println!("🍫🍫🍫🍫🍫🍫🍫🍫Setting up tests");
 }
