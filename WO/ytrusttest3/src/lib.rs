598f1fa0dbf24e77f3879dce71ad2b485f43afa0
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-03 10:40:36 +0000

    🫦

--- WO/ytrusttest3/src/lib.rs
+++ WO/ytrusttest3/src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+fn sub(left: u64, right: u64) -> u64 {
+    left - right
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -10,5 +14,11 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_works_sub() {
+        let result = sub(2, 2);
+        assert_eq!(result, 0);
     }
 }
