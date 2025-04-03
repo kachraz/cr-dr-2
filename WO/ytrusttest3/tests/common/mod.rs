634e799d857eca1938b1f59e91fe412a91906b89
kachraz <110726496+kachraz@users.noreply.github.com>
2025-04-03 17:15:22 +0000

    🫦

--- WO/ytrusttest3/tests/common/mod.rs
+++ WO/ytrusttest3/tests/common/mod.rs
@@ -29,13 +29,21 @@ const TEXT: &str = "\
 //     Ok(())
 // }
 
+/*
+pub fn new(
+gradient: Option<Box<dyn colorgrad::Gradient>>,
+ns: Option<Box<dyn noise::NoiseFn<f64, 2>>>
+) -> Self
+- This is the proper way to use
+*/
+
 fn mecol() -> Result<(), Box<dyn std::error::Error>> {
     let stdout = io::stdout();
     let mut stdout = stdout.lock();
 
     let mut lol = Lolcrab::new(None, None);
     lol.gradient = Box::new(colorgrad::preset::plasma());
-    lol.noise = Box::new(noise::17);
+    lol.ns = Box::new(noise::NoiseFn::(0.0, 0.0));
 
     lol.colorize_str(TEXT, &mut stdout)?;
 
